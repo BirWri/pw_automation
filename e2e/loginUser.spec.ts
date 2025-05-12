7ea31d5bad8c33dbdd2bfd231e4499ad0ec270a1
BirWri <wrightbirgit@gmail.com>
2025-05-12 18:34:26 +0200

    adding login with wrong email

--- e2e/loginUser.spec.ts
+++ e2e/loginUser.spec.ts
@@ -2,10 +2,12 @@ import { test, expect } from '@playwright/test';
 import fs from 'fs';
 import HomePage from '../pages/home.page';
 
+test.describe('Login user, Login with wrong email', () => {
+
 const user = JSON.parse(fs.readFileSync('test-user.json', 'utf-8'));
 let homePage: HomePage;
 
-test('test', async ({ page }) => {
+test('Login - successful', async ({ page }) => {
 homePage= new HomePage(page);
     await page.goto('https://www.automationexercise.com/');
     await homePage.consentButton.click();
@@ -18,4 +20,22 @@ homePage= new HomePage(page);
 
     // Check that correct user is logged in
     await expect(page.locator('text=Logged in as')).toContainText(user.userName);
+});
+
+test('Login - wrong password', async ({ page }) => {
+homePage= new HomePage(page);
+    await page.goto('https://www.automationexercise.com/');
+    await homePage.consentButton.click();
+    await page.getByRole('link', { name: ' Signup / Login' }).click();
+    await homePage.loginEmail.click();
+    await homePage.loginEmail.fill(user.userEmail);
+    await homePage.loginPassword.click();
+    await homePage.loginPassword.fill('password');
+    await homePage.loginButton.click();
+
+    // Check that correct user is logged in
+    await expect(page.locator('text=Your email or password is incorrect!')).toBeVisible();
+});
+
+
 });
